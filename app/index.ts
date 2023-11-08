b1bd4a5178052102d458aec1beb902c33bd37e4f
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2023-11-08 12:52:17 +0300

    initial project setup

---
+++ app/index.ts
@@ -0,0 +1,62 @@
+import 'graphql-import-node';
+import fastify from 'fastify';
+import {
+  getGraphQLParameters,
+  processRequest,
+  renderGraphiQL,
+  Request,
+  sendResult,
+  shouldRenderGraphiQL,
+} from 'graphql-helix';
+import { execute, parse } from 'graphql';
+import { schema } from './schema';
+
+async function app() {
+  const server = fastify();
+  const port = 3001;
+  server.route({
+    // method: ['POST', 'GET'],
+    method: ['POST', 'GET'],
+    url: '/graphql',
+    handler: async (req, resp) => {
+      const request: Request = {
+        headers: req.headers,
+        method: req.method,
+        query: req.query,
+        body: req.body,
+      };
+
+      if (shouldRenderGraphiQL(request)) {
+        resp.header('Content-Type', 'text/html');
+        resp.send(
+          renderGraphiQL({
+            endpoint: '/graphql',
+          })
+        );
+
+        return;
+      }
+      const { operationName, query, variables } = getGraphQLParameters(request);
+
+      const result = await processRequest({
+        request,
+        schema,
+        operationName,
+        query,
+        variables,
+      });
+
+      sendResult(result, resp.raw);
+    },
+  });
+
+  server.listen({ port: port }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+}
+
+app();
